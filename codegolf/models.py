bb6814eee3441608f03480c11d05e067f25e37fb
wisebaldone <ehugh1@gmail.com>
2016-02-11 14:13:14 +1000

    fixed copy-paste error

--- codegolf/models.py
+++ codegolf/models.py
@@ -5,6 +5,6 @@ from codegolf import db
 
 class User(db.Model):
     id = db.Column(db.INTEGER, primary_key=True)
-    name = db.Column(db.TEXT, primary_key=True)
-    email = db.Column(db.TEXT, primary_key=True)
-    uqName = db.Column(db.TEXT, primary_key=True)
+    name = db.Column(db.TEXT)
+    email = db.Column(db.TEXT)
+    uqName = db.Column(db.TEXT)
