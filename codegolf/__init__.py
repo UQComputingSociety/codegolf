567aa141546c905b842949799474742bf171a445
Tom Manderson <TRManderson@gmail.com>
2016-02-15 20:38:34 +1000

    Update app decl to use sqlalchemy rather than flask_sqlalchemy properly

--- codegolf/__init__.py
+++ codegolf/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
+import sqlalchemy as sa
 from .models import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+Session = sa.sessionmaker(bind=engine)
+
+# instantiate Session to do things.
+# I might write some nice Flask extension here, but I don't want to use flask_sqlalchemy
 
 from codegolf import views
