0e21984d71a558e79b75e561ac8511576b539d07
wisebaldone <ehugh1@gmail.com>
2016-02-16 20:53:53 +1000

    Removed the old models file and fixed mistype error

--- codegolf/__init__.py
+++ codegolf/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 import sqlalchemy as sa
+import sqlalchemy.orm as sao
 from .models import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-Session = sa.sessionmaker(bind=engine)
+Session = sao.sessionmaker(bind=engine)
 
 # instantiate Session to do things.
 # I might write some nice Flask extension here, but I don't want to use flask_sqlalchemy
